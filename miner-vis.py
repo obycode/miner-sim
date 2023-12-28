8e849049cfdbcd9abbff009cd13daa1373668c81
Brice Dobry <brice@hiro.so>
2023-12-28 12:03:22 -0500

    remove unnecessary queries and display spends

--- miner-vis.py
+++ miner-vis.py
@@ -6,11 +6,17 @@ def get_block_commits_with_parents(db_file, last_n_blocks=1000):
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
 
+    # Pre-compute the maximum block height
+    cursor.execute("SELECT MAX(block_height) FROM block_commits")
+    max_block_height = cursor.fetchone()[0]
+    lower_bound_height = max_block_height - last_n_blocks
+
     # Fetch the necessary data to build the graph
     query = """
     SELECT
         block_header_hash,
         sortition_id,
+        vtxindex,
         block_height,
         burn_fee,
         parent_block_ptr,
@@ -18,42 +24,33 @@ def get_block_commits_with_parents(db_file, last_n_blocks=1000):
     FROM
         block_commits
     WHERE
-        block_height > (SELECT MAX(block_height) FROM block_commits) - ?
+        block_height > ?
     ORDER BY
         block_height ASC
     """
-    cursor.execute(query, (last_n_blocks,))
+    cursor.execute(query, (lower_bound_height,))
     commits = cursor.fetchall()
 
-    # Fetch parent block_header_hash using parent_block_ptr and parent_vtxindex
-    parents_query = """
-    SELECT
-        block_header_hash
-    FROM
-        block_commits
-    WHERE
-        block_height = ? AND vtxindex = ?
-    """
     # Prepare a dictionary to hold the parent hashes
     parent_hashes = {}
-    for _, _, _, _, parent_block_ptr, parent_vtxindex in commits:
-        if (parent_block_ptr, parent_vtxindex) not in parent_hashes:
-            cursor.execute(parents_query, (parent_block_ptr, parent_vtxindex))
-            result = cursor.fetchone()
-            parent_hashes[(parent_block_ptr, parent_vtxindex)] = result[0] if result else None
+    sortition_sats = {}
+    for block_header_hash, sortition_id, vtxindex, block_height, burn_fee, _, _ in commits:
+        parent_hashes[(block_height, vtxindex)] = block_header_hash
+        sortition_sats[sortition_id] = sortition_sats.get(sortition_id, 0) + int(burn_fee)
 
     conn.close()
-    return commits, parent_hashes
+    return commits, parent_hashes, sortition_sats
 
-def create_graph(commits, parent_hashes):
+def create_graph(commits, parent_hashes, sortition_sats):
     dot = Digraph(comment='Mining Status')
 
     # Group nodes by sortition_id and create edges to parent nodes
-    for block_header_hash, sortition_id, block_height, burn_fee, parent_block_ptr, parent_vtxindex in commits:
+    for block_header_hash, sortition_id, _, block_height, burn_fee, parent_block_ptr, parent_vtxindex in commits:
         print(f'Block Header Hash: {block_header_hash}')
         with dot.subgraph(name=f'cluster_{sortition_id}') as c:
-            c.attr(label=f'Sortition ID: {sortition_id}\nBlock Height: {block_height}')
-            c.node(block_header_hash, f'{block_header_hash}\Sats Spent: {burn_fee}')
+            sortition_spend = sortition_sats.get(sortition_id, 0)
+            c.attr(label=f'Block Height: {block_height}\nTotal Spend: {sortition_spend}')
+            c.node(block_header_hash, f'{block_header_hash[:8]}\nSpend: {int(burn_fee)/sortition_spend:.2%}')
             parent_hash = parent_hashes.get((parent_block_ptr, parent_vtxindex))
             if parent_hash:
                 c.edge(parent_hash, block_header_hash)
@@ -67,6 +64,6 @@ if __name__ == "__main__":
 
     db_path = sys.argv[1]
     last_n_blocks = int(sys.argv[2])
-    commits, parent_hashes = get_block_commits_with_parents(db_path, last_n_blocks)
+    commits, parent_hashes, sortition_sats = get_block_commits_with_parents(db_path, last_n_blocks)
 
-    create_graph(commits, parent_hashes)
+    create_graph(commits, parent_hashes, sortition_sats)
